14db6f5d62ab409107283957e9cd6d0904a1f5fa
bilyuda2112 <bilyuda2112@gmail.com>
2024-12-04 20:08:28 +0000

    commit semua

--- lib/api/config/config.php
+++ lib/api/config/config.php
@@ -1,3 +1,9 @@
-<?php
+ <?php
 $koneksi = mysqli_connect('localhost', 'root', '', 'api_uas');
+
+// Cek koneksi
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil!";
 ?>
